c2e22c8854fe106469618e716b0ecb6a5dbcefbd
Fianko Buckle <kingfi@fb.com>
2020-06-23 15:09:20 -0700

    create Recycler View Adapter class and implemented it in the MainActivity class

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +15,32 @@ public class MainActivity extends AppCompatActivity {
 
     List<String> items;
 
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+
         items = new ArrayList<>();
         items.add("Buy Milk");
         items.add("go to store");
-        
+
+        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+
+
 
     }
 }
